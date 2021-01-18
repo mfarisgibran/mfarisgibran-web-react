0ab89732e79b5e33703ab8140f70bd9a21337506
M Faris Gibran <mfarisgibran@gmail.com>
2021-01-18 20:36:07 +0700

    Improve navigation with logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../images/mfarisgibran-logo.svg'
 
 export default function Navigation() {
   return (
     <nav>
-      <h1>M Faris Gibran</h1>
+      <Link to="/">
+        <img src={Logo} alt="logo" />
+        <h1>M Faris Gibran</h1>
+      </Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
     </nav>
   )
 }
