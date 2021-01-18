24c85771da64b09c5f9bcd5275cf182ba4e15f35
M Faris Gibran <mfarisgibran@gmail.com>
2021-01-18 20:08:49 +0700

    Rename files in pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
+import Home from './pages/home'
+import About from './pages/about'
+import Contact from './pages/contact'
+
 export default function App() {
   return (
     <Router>
       <nav>
         <h1>M Faris Gibran</h1>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
       </nav>
     </Router>
   )
