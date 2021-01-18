7d51f58d583d96768e7a42c176d5628273ac41a5
M Faris Gibran <mfarisgibran@gmail.com>
2021-01-18 21:13:15 +0700

    Create contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,26 @@
 const Contact = () => {
-  return <div>This is contact page</div>
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <form>
+        <div>
+          <label htmlFor="name">Your Name</label>
+          <input type="text" placeholder="Daniel Schwarz" />
+        </div>
+        <div>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" placeholder="daniel.schwarz@mail.com" />
+        </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea name="message" cols="30" rows="10" />
+        </div>
+        <div>
+          <input type="submit" value="Send message" />
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default Contact
