1918205f4b63d74bca646b551033e2d28cb63b50
M Faris Gibran <mfarisgibran@gmail.com>
2021-01-18 21:06:53 +0700

    Style navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,46 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Logo from '../images/mfarisgibran-logo.svg'
+import logoImage from '../images/mfarisgibran-logo.svg'
+import styled from 'styled-components'
+
+const Nav = styled.nav`
+  background-color: #473720;
+  display: flex;
+  justify-content: space-between;
+  padding: 0px 20px;
+  a {
+    color: #fff;
+    text-decoration: none;
+  }
+`
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  h1 {
+    margin-left: 10px;
+  }
+`
+const NavLinks = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    margin-left: 10px;
+  }
+`
 
 export default function Navigation() {
   return (
-    <nav>
+    <Nav>
       <Link to="/">
-        <img src={Logo} alt="logo" />
-        <h1>M Faris Gibran</h1>
+        <Logo>
+          <img src={logoImage} alt="logo" height={18} />
+          <h1>M Faris Gibran</h1>
+        </Logo>
       </Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
-    </nav>
+      <NavLinks>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </NavLinks>
+    </Nav>
   )
 }
